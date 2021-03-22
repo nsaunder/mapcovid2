50bbc9e8f5103f958bae40cf79b431ff3ce712fc
ctoffa <ctoffa@usc.edu>
2021-03-22 02:54:16 -0600

    Onboarding Scroll

---
+++ app/src/main/java/com/example/mapcovid/Onboarding.java
@@ -0,0 +1,34 @@
+package com.example.mapcovid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.mapcovid.ui.path.PathViewModel;
+
+public class Onboarding extends Fragment {
+
+    private Button b1;
+    private PathViewModel mViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.onboarding_scroll_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PathViewModel.class);
+        // TODO: Use the ViewModel
+    }
+}
