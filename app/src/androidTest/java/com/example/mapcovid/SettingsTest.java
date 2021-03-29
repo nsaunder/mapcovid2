0bd05a50cd7ba2180f1aea0f6a95c9cf8b3c700b
Rahul Mehta <rkmehta@usc.edu>
2021-03-29 00:40:15 -0700

    fixes

--- app/src/androidTest/java/com/example/mapcovid/SettingsTest.java
+++ app/src/androidTest/java/com/example/mapcovid/SettingsTest.java
@@ -1,42 +1,30 @@
 package com.example.mapcovid;
 
-import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.rule.GrantPermissionRule;
-import androidx.test.uiautomator.UiDevice;
-import androidx.test.uiautomator.UiObject;
-import androidx.test.uiautomator.UiObjectNotFoundException;
-import androidx.test.uiautomator.UiSelector;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.scrollTo;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-
-
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 @RunWith(AndroidJUnit4.class)
-@LargeTest
 public class SettingsTest {
+
+    private SettingsTest settingstest;
 
     @Rule
     public ActivityTestRule<HomeActivity> mHomeRule =
@@ -44,12 +32,10 @@ public class SettingsTest {
     @Rule
     public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
 
-
     @Before
     public void initialize() {
         DatabaseReference mockDB = Mockito.mock(DatabaseReference.class);
         FirebaseDatabase mockFDB = Mockito.mock(FirebaseDatabase.class);
-
         Constant constants = new Constant();
         Constant spy = Mockito.spy(constants);
         Mockito.when(mockFDB.getReference()).thenReturn(mockDB);
@@ -58,7 +44,6 @@ public class SettingsTest {
 
     @After
     public void after() {
-
     }
 
     @Test
